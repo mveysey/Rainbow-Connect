9aa237b6955a1794ab4cc9625c69ef34e96e650a
mveysey <71096754+mveysey@users.noreply.github.com>
2021-06-26 13:47:25 -0400

    6

--- Rainbow Connect/Assets/_Scripts/Menus/MakeProfile.cs
+++ Rainbow Connect/Assets/_Scripts/Menus/MakeProfile.cs
@@ -1,14 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MakeProfile : MonoBehaviour
 {
     PlayerData currentPlayerData = new PlayerData(); // New player data by default
 
+    public InputField nameText;
+    public InputField pronounsText;
+    public InputField genderText;
+
     // MENU BUTTONS 
 
-    public void Submit() {
+    public void ChangeGender(string gender) {
+        currentPlayerData.gender = gender;
+        pronounsText.text = PlayerData.GetPronouns(gender);
+    }
 
+    public void ChangeLookingForLove(bool isLookingForLove) {
+        currentPlayerData.isHereForLove = isLookingForLove;
+    }
+
+    public void ChangeRoomType(bool isSameRoom) {
+        currentPlayerData.isSameRoomType = isSameRoom;
+    }
+
+    public void Submit() {
+        currentPlayerData.name = nameText.text;
+        currentPlayerData.pronouns = pronounsText.text;
+
+        if (currentPlayerData.gender == "other") {
+            currentPlayerData.gender = genderText.text;
+        }
+
+        print(currentPlayerData.name);
+        print(currentPlayerData.pronouns);
+        print(currentPlayerData.gender);
+        print(currentPlayerData.isHereForLove);
+        print(currentPlayerData.isSameRoomType);
+
+        // change scene? all the relevant attributes are stored in the "currentPlayerData" variable
     }
 }
